9bc19e970be7b1170db982ca46324ff448f96a90
hyc <hyc554@outlook.com>
2019-01-27 15:52:35 +0800

    leetcode-2

---
+++ leetcode/linklist/2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author    : Young
+@Email     : hyc554@outlook.com
+@site      : http://www.cnblogs.com/huang-yc/
+@File      : 2.py.py
+@version   : 1.0
+@Time      : 2019/1/27 10:30
+Description about this file: 
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = ListNode(0)
+        r = res
+        carry = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            s = carry + x + y
+            carry = s // 10
+            r.next = ListNode(s % 10)
+            r = r.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+            if carry > 0:
+                r.next = ListNode(1)
+        return res.next
