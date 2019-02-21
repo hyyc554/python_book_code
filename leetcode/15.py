372e5af6a37d90b40489ab6c0a585142d8520e15
hyc <hyc554@outlook.com>
2019-02-21 21:09:20 +0800

    leetcode15

---
+++ leetcode/15.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author    : Young
+@Email     : hyc554@outlook.com
+@site      : http://www.cnblogs.com/huang-yc/
+@File      : 15.py.py
+@version   : 1.0
+@Time      : 2019/2/21 20:13
+Description about this file:
+
+"""
+
+
+class Solution:
+    def threeSum(self, nums):
+
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
+
+
+data = [1, 2, -1, -2, 3, 0, 5, -3]
+sk = Solution()
+print(sk.threeSum(nums=data))
