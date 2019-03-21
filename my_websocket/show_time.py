43b5f00855341fae9c627851a80367df10cdeaf2
hyc <hyc554@outlook.com>
2019-03-21 11:43:02 +0800

    websocket

---
+++ my_websocket/show_time.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author    : Young
+@Email     : hyc554@outlook.com
+@site      : http://www.cnblogs.com/huang-yc/
+@File      : show_time.py
+@version   : 1.0
+@Time      : 2019/3/21 10:10
+Description about this file:
+
+"""
+
+
+#!/usr/bin/env python
+
+# WS server that sends messages at random intervals
+
+import asyncio
+import datetime
+import random
+import websockets
+
+async def time(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        await websocket.send(now)
+        await asyncio.sleep(random.random() * 3)
+
+start_server = websockets.serve(time, '127.0.0.1', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
