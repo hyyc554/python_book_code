c6bd28c71390a06f74eeb5f0c24d0fd5e186162a
hyc <1415940604@qq.com>
2019-03-26 23:00:07 +0800

    反转链表——迭代

--- leetcode/linklist/reverse.py
+++ leetcode/linklist/reverse.py
@@ -24,6 +24,11 @@ class Node(object):
 
 
 def reverse_loop(head):
+    """
+    循坏迭代
+    :param head:
+    :return:
+    """
     if not head or not head.next:
         return head
     pre = None
@@ -33,7 +38,20 @@ def reverse_loop(head):
         pre = head          # 作为下次迭代时的（当前节点的）向前指针
         head = next         # 作为下次迭代时的（当前）节点
     return pre              # 返回头指针，头指针就是迭代到最后一次时的head变量（赋值给了pre）
+def reverse_recursion(head):
+    """
+    递归法
+    :param head:
+    :return:
+    """
+    if not head or not head.next:
+        return head
 
+    new_head = reverse_recursion(head.next)
+
+    head.next.next = head
+    head.next = None
+    return new_head
 
 if __name__ == '__main__':
 
@@ -52,7 +70,7 @@ if __name__ == '__main__':
     head.value = 0
     head.next = one
 
-    newhead = reverse_loop(head)
+    newhead = reverse_recursion(head)
     while newhead:
         print(newhead.value, )
         newhead = newhead.next
