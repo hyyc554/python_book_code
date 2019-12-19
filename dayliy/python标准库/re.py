c0a74776593f87b383c649e63db4d51c04cba629
hyyc554 <1415940604@qq.com>
2019-12-19 13:45:40 +0800

    12月日常

--- dayliy/python标准库/re.py
+++
@@ -1,7 +0,0 @@
-import re
-pattern = "this"
-text = 'Does this text match the pattern'
-match = re.search(pattern,text)
-s = match.start()
-e = match.end()
-print(f"Found \"{match.re.pattern}\" in \"{match.string}\" from \"{s}\" to \"{e}\" ({text[s:e]})")
